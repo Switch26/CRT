9a3a19d0ff84df0be39dd0fd5bb73417823d675c
Switch26 <sergueivinnitskii@gmail.com>
2022-09-25 12:06:32 -0400

    successful connect to Docker Cassandra named cassandra_some_network

--- entryPoint.go
+++ entryPoint.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+//The package name can be an arbitrary identifier, though if you want a package to serve as an entry point for an executable program, it needs to be named “main” and have a function main() with no arguments and no return type.
+
+import (
+	"fmt"
+	"github.com/gocql/gocql"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+
+	cluster := gocql.NewCluster("cassandra_some_network")
+	cluster.Keyspace = "links_keyspace"
+	cluster.Consistency = gocql.Quorum
+
+	if _, err := cluster.CreateSession(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connection successfull...")
+	fmt.Println("hahaha")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module CRT
 
 go 1.18
+
+require (
+	github.com/gocql/gocql v1.2.1 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+)
